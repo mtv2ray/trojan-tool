284928724f3454a216eae431fafc0541ad0527a4
metoo5566 <351757417@qq.com>
2023-07-03 17:54:12 +0800

    123

--- test.sh
+++ test.sh
@@ -4,3 +4,7 @@ if [ "$1" != "" ];then
     DOMAIN_NAME="$1"
 fi
 echo "$DOMAIN_NAME"
+
+MAINASSET="https://raw.githubusercontent.com/mtv2ray/trojan-tool/main"
+curl -o "$DOMAIN_NAME.json.doc" "$MAINASSET/sspanel.json"
+sed "s/{DOMAIN}/$DOMAIN_NAME/g" "$DOMAIN_NAME.json.doc" > "$DOMAIN_NAME.json"
